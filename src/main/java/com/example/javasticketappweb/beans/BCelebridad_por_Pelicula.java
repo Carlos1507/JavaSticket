019247835f702e3416a96448158bdcb2774350ce
raykevinconcha <tudela2352>
2022-06-03 23:07:22 -0500

    cpor_pelicula

---
+++ src/main/java/com/example/javasticketappweb/beans/BCelebridad_por_Pelicula.java
@@ -0,0 +1,22 @@
+package com.example.javasticketappweb.beans;
+
+public class BCelebridad_por_Pelicula {
+    private int idCelebridad;
+    private int idPelicula;
+
+    public int getIdCelebridad() {
+        return idCelebridad;
+    }
+
+    public void setIdCelebridad(int idCelebridad) {
+        this.idCelebridad = idCelebridad;
+    }
+
+    public int getIdPelicula() {
+        return idPelicula;
+    }
+
+    public void setIdPelicula(int idPelicula) {
+        this.idPelicula = idPelicula;
+    }
+}
