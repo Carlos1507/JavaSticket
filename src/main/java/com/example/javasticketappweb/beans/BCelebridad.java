61df5a213ddc44dbcbba5b813442e9320eb28ae6
David Gonzales <dgonzalesn@pucp.edu.pe>
2022-06-03 22:39:52 -0500

    Celebridad Creada

---
+++ src/main/java/com/example/javasticketappweb/beans/BCelebridad.java
@@ -0,0 +1,49 @@
+package com.example.javasticketappweb.beans;
+
+public class BCelebridad {
+    private int idCelebridad;
+    private String nombre;
+    private String apellido;
+    private String rol;
+    private int foto;
+
+    public int getIdCelebridad() {
+        return idCelebridad;
+    }
+
+    public void setIdCelebridad(int idCelebridad) {
+        this.idCelebridad = idCelebridad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+}
